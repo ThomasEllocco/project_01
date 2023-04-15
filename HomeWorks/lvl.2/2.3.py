eaa9df13c6bf1b46630da13806829544bd00ea38
ThomasEllocco <smith.roger@internet.ru>
2023-04-15 20:17:28 +0300

    ADD LVL_2

---
+++ HomeWorks/lvl.2/2.3.py
@@ -0,0 +1,20 @@
+# Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя!
+
+def switch_it_up(number):
+    number = int(input("Введите цифру: "))
+    try:
+        a = [
+            'zero', 'one', 'two', 'three', 'four',
+             'five', 'six', 'seven', 'eight', 'nine'
+             ]
+        return a[number]
+    except IndexError:
+        return None
+print(switch_it_up(5))
